f60bbe79bbbaaa764c35363c12dec48af57e2381
Njiva Randriamiarisoa <njiva.randriamiarisoa>
2018-11-20 10:13:27 +0300

    master:save

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get '/propos', to: 'about#propos', as: :propos
 
-  root 'home#index'
+  get '/services', to: 'services#services', as: :services
+
+  root 'home#index', as: :home
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
